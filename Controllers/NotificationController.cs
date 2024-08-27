e53fb06eaf0e9733e27763cbcd0d47467feb9db3
Chathushi <chathushikavindya09@gmail.com>
2024-08-27 12:08:09 +0530

    modify notification in driver

--- Controllers/NotificationController.cs
+++ Controllers/NotificationController.cs
@@ -1,3 +1,4 @@
+using FleetPulse_BackEndDevelopment.DTOs;
 using FleetPulse_BackEndDevelopment.Models;
 using FleetPulse_BackEndDevelopment.Services.Interfaces;
 using Microsoft.AspNetCore.Authorization;
@@ -67,6 +68,37 @@ namespace FleetPulse_BackEndDevelopment.Controllers
             // Simulate deleting all notifications in memory
             await _pushNotificationService.DeleteAllNotificationsAsync();
             return Ok(new { Status = "Success", Message = "All notifications deleted" });
+        }
+        
+        [HttpPost("send-reset-password-notification")]
+        public async Task<IActionResult> SendResetPasswordNotification([FromBody] FCMNotificationDTO notificationDto)
+        {
+            if (notificationDto == null || string.IsNullOrEmpty(notificationDto.EmailAddress))
+            {
+                return BadRequest("Invalid notification data.");
+            }
+
+            var notification = new FCMNotificationDTO
+            {
+                NotificationId = Guid.NewGuid().ToString(),
+                Username = notificationDto.Username,
+                JobTitle = notificationDto.JobTitle,
+                Title = "Password Reset Request",
+                Message = "User has requested a password reset.",
+                Date = DateTime.UtcNow,
+                Time = DateTime.UtcNow.TimeOfDay,
+                EmailAddress = notificationDto.EmailAddress,
+                Status = false
+            };
+
+            var result = await _pushNotificationService.SendNotificationAsynctoAdmin(notification);
+
+            if (result)
+            {
+                return Ok("Notification sent successfully.");
+            }
+
+            return StatusCode(500, "Error sending notification.");
         }
         
     }
--- Services/PushNotificationService.cs
+++ Services/PushNotificationService.cs
@@ -158,6 +158,7 @@ namespace FleetPulse_BackEndDevelopment.Services
             }
         }
 
+        
         public async Task<bool> SendNotificationAsynctoAdmin(FCMNotificationDTO notification)
         {
             var emailExists = DoesEmailExist(notification.EmailAddress);
@@ -189,26 +190,15 @@ namespace FleetPulse_BackEndDevelopment.Services
                     Title = notification.Title,
                     Body = notification.Message
                 },
-                Token =
-                    "foAwll9oGeXgr1eS7d0h-w:APA91bFORCNY1m8DQjVql0g14z64BEvuncpVuh5JKqkPxILLqwJBqg_B-4MZqpVI-gPISSy6c-py-ioprh45M4MezQQwYDN5EkejBTH7SdiRLUbU6VUoaJQrbgL1cJDK8jI-0PlS3tot" // replace with the actual admin device token
+                Token = "foAwll9oGeXgr1eS7d0h-w:APA91bFORCNY1m8DQjVql0g14z64BEvuncpVuh5JKqkPxILLqwJBqg_B-4MZqpVI-gPISSy6c-py-ioprh45M4MezQQwYDN5EkejBTH7SdiRLUbU6VUoaJQrbgL1cJDK8jI-0PlS3tot" // replace with the actual admin device token
             };
 
-            _logger.LogInformation(
-                $"Sending notification with data: {{Username: {notification.Username}, JobTitle: {notification.JobTitle}, Title: {notification.Title}, Message: {notification.Message}, EmailAddress: {notification.EmailAddress}}}");
+            _logger.LogInformation($"Sending notification with data: {{Username: {notification.Username}, JobTitle: {notification.JobTitle}, Title: {notification.Title}, Message: {notification.Message}, EmailAddress: {notification.EmailAddress}}}");
 
             try
             {
                 var response = await FirebaseMessaging.DefaultInstance.SendAsync(message);
                 _logger.LogInformation("Successfully sent message: " + response);
-                await SaveNotificationAsync(new FCMNotification
-                {
-                    NotificationId = Guid.NewGuid().ToString(),
-                    Title = notification.Title,
-                    Message = notification.Message,
-                    Date = DateTime.UtcNow,
-                    Time = DateTime.UtcNow.TimeOfDay,
-                    Status = false
-                });
                 return true;
             }
             catch (Exception ex)
